833ead9fcc478be0bf53bdb0ca33f9308abca8f8
ofriedler <ofriedler@outbrain.com>
2024-01-20 15:45:09 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Custom parts price prediction',
+    author='Ophir Friedler',
+    license='',
+)
